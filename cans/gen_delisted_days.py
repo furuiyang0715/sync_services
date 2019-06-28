aa819b730396331bfb95b4dc890020cd7417abbe
furuiyang <furuiyang0715@gmail.com>
2019-06-28 10:22:59 +0800

    增加各类非交易日生成逻辑

---
+++ cans/gen_delisted_days.py
@@ -0,0 +1,124 @@
+import datetime
+import logging
+
+from cans import utils
+from cans.utils import DC
+
+logger = logging.getLogger("delisted")
+
+
+def gen_delisted_info(code, timestamp):
+    """
+    生成某只股票的全部退市时间信息 info dict 形式
+
+    有关状态的说明： 1-上市，2-暂停上市，3-恢复上市，4-终止上市，5-摘牌，6-退市整理期
+
+    +-----------+----------+---------------------+---------------------+------------+
+    | InnerCode | SecuCode | ListedDate          | ChangeDate          | ChangeType |
+    +-----------+----------+---------------------+---------------------+------------+
+    |       368 | 000693   | 1997-02-26 00:00:00 | 1997-02-26 00:00:00 |          1 |
+    |       368 | 000693   | 1997-02-26 00:00:00 | 2007-05-23 00:00:00 |          2 |
+    |       368 | 000693   | 1997-02-26 00:00:00 | 2014-01-10 00:00:00 |          3 |
+    |       368 | 000693   | 1997-02-26 00:00:00 | 2018-07-13 00:00:00 |          2 |
+    |       368 | 000693   | 1997-02-26 00:00:00 | 2019-05-27 00:00:00 |          6 |
+    +-----------+----------+---------------------+---------------------+------------+
+    :param code:
+    :param timestamp: 查询的时间戳
+    :return:
+    """
+
+    conn = DC()
+
+    # 注意如果不加引号的报错： Warning: (1292, "Truncated incorrect DOUBLE value: 'X11098'")
+
+    query_sql = """
+    SELECT A.InnerCode,A.SecuCode,A.ListedDate,B.ChangeDate,B.ChangeType 
+    from stk_liststatus B,const_secumainall A WHERE 
+    A.InnerCode=B.InnerCode 
+    AND A.SecuCode = "{}" AND A.SecuMarket IN(83,90) AND A.SecuCategory=1 
+    AND B.ChangeType IN(1,2,3,4,5,6)
+    and A.UPDATETIMEJZ<= "{}"
+    and B.UPDATETIMEJZ<= "{}";
+    """.format(code, timestamp, timestamp)
+
+    infos = list()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query_sql)
+            res = cursor.fetchall()
+            for column in res:
+                # code = column[1],
+                listed_date = column[2]
+                change_date = column[3]
+                change_type = column[4]
+
+                if change_type in [1, 3]:  # 上市和恢复上市
+                    infos.append({
+                        "code": code,
+                        "listed_date": listed_date,
+                        "change_date": change_date,
+                        "change_type": change_type,
+                        "is_listed": True,
+                    })
+                else:
+                    infos.append({
+                        "code": code,
+                        "listed_date": listed_date,
+                        "change_date": change_date,
+                        "change_type": change_type,
+                        "is_listed": False,
+                    })
+    finally:
+        conn.commit()
+    return infos
+
+
+def gen_delisted_days(infos, limit_date):
+    # 生成某只股票的汇总退市时间列表
+    # 将 infos 按照 change_date 进行排序
+
+    infos = sorted(infos, key=lambda item: item.get("change_date"))
+
+    delisted_days = list()
+
+    for j in range(len(infos)-1):
+        start = infos[j]
+        end = infos[j+1]
+
+        # 分为4种情况 （1） True,  True   数据错误 无暂停即恢复
+        #            （2） True,  False  上市时间  [ ）
+        #            （3） False, False  退市时间  [ ]
+        #            （4） False, True   退市时间  [ )
+
+        if start == end:
+            logger.info("从上市至今未经历停牌.")
+            return list()
+
+        elif start.get("is_listed") and end.get("is_listed"):
+            logger.info("数据错误，未经历退市即又上市")
+
+        elif start.get("is_listed") and not end.get("is_listed"):  # True False
+            # 将最后一天加入 退市时间
+            # delisted_days.append(end.get("change_date"))   # 已有最后一天到截止时间的判断
+            pass
+
+        elif not start.get("is_listed") and not end.get("is_listed"):  # False False
+            delisted_days.extend(utils.get_date_list(start.get("change_date"), end.get("change_date")))
+
+        elif not start.get("is_listed") and end.get("is_listed"):  # False True
+            # 直到重新上市时间的前一天加入退市时间
+            delisted_days.extend(utils.get_date_list(start.get("change_date"), end.get("change_date") -
+                                               datetime.timedelta(days=1)))
+
+    # 从最后一个时间到今天的判断
+    last_info = infos[-1]
+    if last_info.get("is_listed"):
+        pass
+    else:
+        # 将最后一个change_date 到今天的时间加到退市时间里面
+        delisted_days.extend(utils.get_date_list(last_info.get("change_date"), limit_date))
+
+    # 去除重复值
+    # delisted_days = sorted(list(set(delisted_days)))
+
+    return delisted_days
---
+++ cans/gen_sus_days.py
@@ -0,0 +1,96 @@
+import logging
+import sys
+
+from cans import utils
+from cans.utils import DC
+
+logger = logging.getLogger("sus")
+
+
+def gen_inc_code_sus(code, start, end, timestamp):
+    """
+    start 和 end 之间的某只股票的全部停牌日 包含 strat 和 end
+    :param code:
+    :param start:
+    :param end:
+    :param timestamp: 进行本次查询的时间戳
+    :return:
+    """
+
+    # 分为两种情况：
+    # （1）在首次更新的时候，我们会在一段较长的 strat 和 end 之间寻找作为时间段存在的 notice_start 和 notice_end
+    #  (2) 在增量更新的时候我们往往会判断一个较短的时间段（start 到 end) 是否处于某个停牌期
+    #
+    # 在第（1） 种情况下， 要想和一段较长的时间 start 和 end 有交集
+    #
+    # 必须满足：
+    #
+    # notice_end> start and notice_start < end
+    #
+    # 其中 notice_end 可能为 NULL， 说明停牌到今天，此时就将停牌日设置为 end
+
+    if start > end:
+        return
+
+    suspended = list()
+
+    conn = DC()
+
+    query_sql = f"""
+    select id, NoticeStartDate, NoticeEndDate from stk_specialnotice_new
+    where SecuCode = {code} and NoticeTypei = 18 and NoticeTypeii != 1703 
+    and NoticeStartDate <= '{end}'
+    and UPDATETIMEJZ <= '{timestamp}'
+--     and NoticeEndDate >= '{start}'
+    ;"""
+
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query_sql)
+            res = cursor.fetchall()
+
+            if not res:
+                return list()
+
+            for column in res:
+                notice_start = column[1]
+                notice_end = column[2]
+
+                # 对于数据存在以及合理性的判断 (1) 无 notice start 是错误数据 （2） 无 notice end 置为 end
+                # （3） 开始不能大于 结束
+                if not notice_start:
+                    logger.info("no notice start date, wrong.")
+                    continue
+
+                if not notice_end:
+                    notice_end = end
+
+                if notice_end < notice_start:
+                    logger.info(f"notice_start > notice_end, something has been wrong. code is {code}")
+                    sys.exit(1)
+
+                # check相交的情况：
+                if notice_start == notice_end:
+                    # 四点为同一天
+                    suspended.append(notice_start)
+
+                elif notice_end <= end:
+                    # 停牌的起止完全包含于所给时间的情况
+                    suspended.extend(utils.get_date_list(notice_start, notice_end))
+
+                elif notice_start <= start:
+                    # 停牌日从 start 到 notice_end
+                    suspended.extend(utils.get_date_list(start, notice_end))
+
+                elif notice_end >= end:
+                    # 停牌日从 notice_start 到 end
+                    suspended.extend(utils.get_date_list(notice_start, end))
+
+                else:
+                    # 包含 NoticeEndDate >= '{start}'
+                    logger.info(f'未知情况， 未做处理. {code}, {notice_start}, {notice_end}')
+                    sys.exit(1)
+    finally:
+        conn.commit()
+    suspended = sorted(list(set(suspended)))
+    return suspended
